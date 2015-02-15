eda2b05a50b39740bb93802a233a90b5bce9e062
wadror <wadror@wadror-VirtualBox.(none)>
2015-02-15 21:15:13 +0200

    ex2

---
+++ lib/beermapping_api.rb
@@ -0,0 +1,19 @@
+class BeermappingApi
+  def self.places_in(city)
+    url = "http://beermapping.com/webservice/loccity/#{key}/"
+
+    response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
+    places = response.parsed_response["bmp_locations"]["location"]
+
+    return [] if places.is_a?(Hash) and places['id'].nil?
+
+    places = [places] if places.is_a?(Hash)
+    places.inject([]) do | set, place |
+      set << Place.new(place)
+    end
+  end
+
+  def self.key
+    "cb9efb534ed50095208bb4eb7dd52e5e"
+  end
+end
